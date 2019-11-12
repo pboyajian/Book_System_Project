5bacfab08758a3b96c07612379b2ab48ebcd3407
Zach Merel <zachmerel@gmail.com>
2019-11-12 15:07:28 -0600

    started JPA on the note

---
+++ note-service/src/main/java/com/trilogyed/noteservice/controller/NoteController.java
@@ -0,0 +1,9 @@
+package com.trilogyed.noteservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class NoteController {
+
+    @Autowired
+    private NoteRepository noteRepo;
+}
