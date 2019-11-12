c65cf20eb75752d3714953ee5701ae8f29ee4157
Bedros <boyajian@uchicago.edu>
2019-11-12 13:35:22 -0600

    setup

---
+++ book-service-eureka/src/test/java/com/trilogyed/bookserviceeureka/BookServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.bookserviceeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
