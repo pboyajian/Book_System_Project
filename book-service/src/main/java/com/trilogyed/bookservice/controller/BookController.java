d00a68f26447f225818d8d5a5b56b73d55dec943
Bedros <boyajian@uchicago.edu>
2019-11-12 14:53:12 -0600

    producer matching tutorial; everything working

---
+++ book-service/src/main/java/com/trilogyed/bookservice/controller/BookController.java
@@ -0,0 +1,37 @@
+package com.trilogyed.bookservice.controller;
+
+import com.trilogyed.bookservice.model.Book;
+import com.trilogyed.bookservice.util.messages.Note;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    public static final String EXCHANGE = "note-exchange";
+    public static final String ROUTING_KEY = "note.#";
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public BookController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @RequestMapping(value = "/book", method = RequestMethod.POST)
+    public String createBook(@RequestBody Book book) {
+        // create message to send to note creation queue
+        Note msg = new Note(book.getBookId(),"Default note.");
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, msg);
+        System.out.println("Message Sent");
+
+        // Now do book creation stuff...
+
+        return "Book Created";
+    }
+}
+
---
+++ book-service/src/main/java/com/trilogyed/bookservice/model/Book.java
@@ -0,0 +1,31 @@
+package com.trilogyed.bookservice.model;
+
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
